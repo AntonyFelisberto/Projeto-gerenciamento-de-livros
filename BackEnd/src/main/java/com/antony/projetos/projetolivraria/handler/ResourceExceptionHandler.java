bfeff0a00471b4ab3c352aa5ad26cfc6dd356e7f
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2022-10-23 14:51:06 -0300

    Finalizando o projeto do backEnd

--- BackEnd/src/main/java/com/antony/projetos/projetolivraria/handler/ResourceExceptionHandler.java
+++ BackEnd/src/main/java/com/antony/projetos/projetolivraria/handler/ResourceExceptionHandler.java
@@ -3,12 +3,13 @@ package com.antony.projetos.projetolivraria.handler;
 import com.antony.projetos.projetolivraria.exceptions.DataIntegrityViolation;
 import com.antony.projetos.projetolivraria.exceptions.ObjectNotFound;
 import com.antony.projetos.projetolivraria.models.StandardError;
+import com.antony.projetos.projetolivraria.models.ValidationError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import javax.servlet.ServletRequest;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -27,6 +28,17 @@ public class ResourceExceptionHandler {
                 HttpStatus.BAD_REQUEST.value(),
                 exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> methodArgumentNotValidException(MethodArgumentNotValidException exception){
+        ValidationError erro = new ValidationError(System.currentTimeMillis(),
+                                                HttpStatus.BAD_REQUEST.value(),
+                                                "Erro na validação de Campos");
+        for(FieldError erros: exception.getBindingResult().getFieldErrors()){
+            erro.addErrors(erros.getField(),erros.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
     }
 
 }
